e2f2791a226c179ba398a67248a081f3574002b5
Owen Larmon <owenlarmon13@gmail.com>
2025-01-21 11:28:17 -0500

    Lab 2 code

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -20,6 +20,7 @@ import itertools
 
 def best_hand(hand):
     """From a 7-card hand, return the best 5 card hand."""
+    # The one-line solution V
     # return max(itertools.combinations(hand,5), key=hand_rank)
 
     best = (0,0,0)
