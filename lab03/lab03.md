be0a856ffa2f015f6cee14535ea488e70c0ae579
Owen Larmon <owenlarmon13@gmail.com>
2025-01-28 12:06:18 -0500

    Some prompt stuff

--- lab03/lab03.md
+++ lab03/lab03.md
@@ -1,2 +1,7 @@
 # Prompt Engineering Process
 
+My first attempt was just to copy and paste most of what was in the demo_agent file. The main changes that I made was the system context, and I altered the names to Player and DM instead of user and assistant. New context: 'You are a dungeon master. Present the user with fantasy scenarios and give them opurtunities to succeed or fail depending on their actions.'
+
+For my second attempt, I wanted to see what impacts changing temperature and max_tokens would have so I changed their values to 0.34 and 50, repectively.
+
+On my third attempt, I wanted the player to be able to fail in whatever action they choose. Instead of using dice rolls like normal DnD, I want the AI to use its own judgement about whether the action will fail or not. Also, I like that the AI gives players a set of potential actions at the end of its responses, but I want the player to know that they still have the ability to make custom typed-out chocies,
--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -6,12 +6,14 @@ from ollama import chat
 from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 
 # Add you code below
-sign_your_name = 'Pulin Agrawal'
-model = ''
-options = {}
-messages = []
-
-
+sign_your_name = 'Owen Larmon'
+model = 'llama3.2'
+options = {'temperature': 0.34, 'max_tokens': 40}
+messages = [
+  {'role': 'system', 'content': 'You are a dungeon master. Present the user with fantasy scenarios.'},
+  {'role': 'system', 'content': 'Allow player decisions to backfire if they are unlikely to succeed.'},
+  {'role': 'system', 'content': 'Give the player a set of potential options but make it clear they are able to type in custom resposes.'}
+]
 # But before here.
 
 options |= {'seed': seed(sign_your_name)}
@@ -19,11 +21,15 @@ options |= {'seed': seed(sign_your_name)}
 while True:
   response = chat(model=model, messages=messages, stream=False, options=options)
   # Add your code below
-
-
-  # But before here.
-  if messages[-1]['content'] == '/exit':
+  message = {'role': 'user', 'content': input('Player: ')}
+  if message['content'] == '/exit':
     break
+  messages.append(message)
+  response = chat(model=model, messages=messages, stream=False, options=options)
+  print(f'DM: {response.message.content}')
+  messages.append({'role': 'assistant', 'content': response.message.content})
+  # But before here.
+  
 
 # Save chat
 with open(Path('lab03/attempts.txt'), 'a') as f:
